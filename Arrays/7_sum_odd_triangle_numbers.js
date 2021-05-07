eb2282dec88572a256a1fe19170d281adffa89d6
grayTurtle01 <the.correo@protonmail.com>
2021-05-07 13:10:10 -0500

    7_sum_row_odd_numbers

---
+++ Arrays/7_sum_odd_triangle_numbers.js
@@ -0,0 +1,17 @@
+// sum the odd numbers of the n row on odd tringle
+
+//                   n    index   r  
+/*     1             1      1     1
+     3   5           2      2     8
+   7   9   11        3      4     27    
+13  15   17   19     4      7     64 
+   
+*/
+
+//Parameters : Number (n row)
+//Result: Number
+//Example: 2 --> 8
+
+function rowSumOddNumbers(row) {  
+  retur row**3
+}
