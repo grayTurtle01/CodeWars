13a928bae5b609b17b8917dd0ed55950e76fe449
grayTurtle01 <the.correo@protonmail.com>
2021-05-15 09:48:15 -0500

    8_basic_operations

---
+++ Mathematics/8_basic_Op.js
@@ -0,0 +1,12 @@
+// create a function how handel basic operations
+
+//Parameters : String, Number, Number
+//Result: Number
+//Example: "+",1,1 --> 2
+
+function basicOp(operation, value1, value2){
+  return eval( value1 + operation + value2)
+  
+}
+
+console.log( foo() )
