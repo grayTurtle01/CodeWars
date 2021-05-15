4ce4f1cde2d1f85cf8dd0232d940fb666710d2a6
grayTurtle01 <the.correo@protonmail.com>
2021-05-15 09:03:38 -0500

    8_even_or_odd

---
+++ Mathematics/8_even_or_odd.js
@@ -0,0 +1,14 @@
+// this function determine if the number is even or odd
+
+//Parameters : Number
+//Result: String
+//Example: 2 --> 'Even'
+
+function even_or_odd(number) {
+  if( number % 2 == 0 )
+    return 'Even'
+  else
+    return 'Odd'
+  
+}
+console.log( foo() )
