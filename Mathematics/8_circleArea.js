4bf7e27677e03db7ee289e18db3420447875537d
grayTurtle01 <the.correo@protonmail.com>
2021-05-25 16:03:31 -0500

    8_circleArea

---
+++ Mathematics/8_circleArea.js
@@ -0,0 +1,14 @@
+// get the area from a circle 
+
+//Parameters : Circle
+//Result: Number
+//Example: new Circle( (0,0), 2 ) --> pi*2**2 
+
+function circleArea(circle){
+  r = circle.radius
+  
+  area = Math.PI * r**2
+  return area 
+}
+
+console.log( )
