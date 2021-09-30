5252a37f71651ae6c96759194512b750a70735b5
grayTurtle01 <the.correo@protonmail.com>
2021-09-30 12:07:16 -0500

    without *.js

--- TypeScript/even_or_odd.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.even_or_odd = void 0;
-function even_or_odd(n) {
-    if (n % 2 == 0)
-        return "Even";
-    return "Odd";
-}
-exports.even_or_odd = even_or_odd;
