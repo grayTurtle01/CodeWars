2139f352357e8f8c3fcb3576267335fd0733dc33
grayTurtle01 <the.correo@protonmail.com>
2021-10-03 15:50:54 -0500

    xo

--- TypeScript/7/test.js
+++ TypeScript/7/test.js
@@ -1,11 +1,19 @@
-function findShort(s) {
-    let words = s.split(" ");
-    let min_length = 1000;
-    for (let word of words) {
-        if (word.length < min_length) {
-            min_length = word.length;
-        }
+"use strict";
+exports.__esModule = true;
+exports.xo = void 0;
+function xo(str) {
+    var x = 0;
+    var o = 0;
+    var char_array = str.split('');
+    for (var _i = 0, char_array_1 = char_array; _i < char_array_1.length; _i++) {
+        var char = char_array_1[_i];
+        if (char.toLowerCase() == 'x')
+            x += 1;
+        else if (char.toLowerCase() == 'o')
+            o += 1;
     }
-    return min_length;
+    if (x == o)
+        return true;
+    return false;
 }
-console.log(findShort("hello world"));
+exports.xo = xo;
