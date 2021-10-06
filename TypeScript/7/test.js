04546cc4f0e5119a17d253fa8c0cdcc2a51f0327
grayTurtle01 <the.correo@protonmail.com>
2021-10-06 09:36:37 -0500

    final 7

--- TypeScript/7/test.js
+++ TypeScript/7/test.js
@@ -1,10 +1,4 @@
-function countBits(n) {
-    let total = 0;
-    // transform decimal-number to binary
-    let bin_str = n.toString(2);
-    // count up bits
-    for (let bit of bin_str)
-        if (bit == '1')
-            total += 1;
-    return total;
+function findOutlier(integers) {
+    return 0;
 }
+console.log(findOutlier([1, 2]));
--- TypeScript/7/test.ts
+++ TypeScript/7/test.ts
@@ -1,13 +1,1 @@
-function countBits(n: number): number {
-  let total:number = 0;
 
-  // transform decimal-number to binary
-  let bin_str = n.toString(2)
-
-  // count up bits
-  for(let bit of bin_str)
-    if(bit == '1')
-      total += 1
-
-  return total
-}
