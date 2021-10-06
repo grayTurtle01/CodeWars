2a781a7edaf41f1af2fad774b93fce1eb8a8fbd1
grayTurtle01 <the.correo@protonmail.com>
2021-10-06 09:34:41 -0500

    findOutLier

---
+++ TypeScript/6/test.js
@@ -0,0 +1,20 @@
+function findOutlier(integers) {
+    let isEvenArray = false;
+    // Find the kind of array
+    if (integers[0] % 2 == 0 && integers[1] % 2 == 0)
+        isEvenArray = true;
+    if (integers[0] % 2 == 0 && integers[2] % 2 == 0)
+        isEvenArray = true;
+    if (integers[1] % 2 == 0 && integers[2] % 2 == 0)
+        isEvenArray = true;
+    // Search the outlier
+    for (let i = 0; i < integers.length; i++) {
+        if (isEvenArray)
+            if (Math.abs(integers[i] % 2) == 1)
+                return integers[i];
+        if (!isEvenArray)
+            if (integers[i] % 2 == 0)
+                return integers[i];
+    }
+}
+console.log(findOutlier([2, -6, 8, -10, -3]));
