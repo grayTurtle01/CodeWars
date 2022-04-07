c493f225f1f0723d52a337389d84cc3622d7c724
grayTurtle01 <the.correo@protonmail.com>
2022-04-07 09:56:43 -0500

    8-isLove

--- Java/8/Katas.java
+++ Java/8/Katas.java
@@ -32,6 +32,13 @@ public class Katas {
         return x * y;
     }
 
+    public static boolean isLove(final int flower1, final int flower2) {
+        if( (flower1 + flower2) % 2 == 1)
+          return true;
+        else
+          return false;
+    }
+
     public static void main(String args[]){
         System.out.println(multiply(7,8));
     }
