46c8b5588d6e939d99076969f402ca5c234807f6
grayTurtle01 <the.correo@protonmail.com>
2022-04-08 23:12:12 -0500

    8: covertBoolean2String

--- Java/8/Katas8.java
+++ Java/8/Katas8.java
@@ -79,6 +79,11 @@ public class Katas8 {
         return false;
 	}
 
+    public static String convert(boolean b){
+        return b? "true": "false";
+    }
+
+
     public static void main(String args[]){
         System.out.println( fakeBin("1234567890"));
     }
