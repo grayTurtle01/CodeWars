7ed5dc440d75214aa10b3f19a9fd0631332ad635
grayTurtle01 <the.correo@protonmail.com>
2022-04-11 08:51:04 -0500

    6: TowerBuilder

---
+++ Java/6/Katas6.java
@@ -0,0 +1,34 @@
+public class Katas6{
+    
+  public static String[] TowerBuilder(int nFloors){
+    String[] result = new String[nFloors];
+    int spaces = nFloors - 1;
+    
+    for(int y = 0; y < nFloors; y++ ){
+        String floor = "";
+        int blocks = y*2 + 1;
+
+        for( int x = 1; x <= spaces; x++)
+            floor += " ";
+
+        for( int x = 1; x <= blocks; x++)
+            floor += "*";
+
+        for( int x = 1; x <= spaces; x++)
+            floor += " ";
+        
+        //~ System.out.println(floor);
+        result[y] = floor;
+
+        spaces--;
+    }
+
+    return result;
+  }
+
+
+  public static void main(String args[]){
+      TowerBuilder(6);
+  }
+  
+}
