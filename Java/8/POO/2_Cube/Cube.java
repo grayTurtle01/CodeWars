a8abc123125c98aa64d6fd47f4ff40f033e5b579
grayTurtle01 <the.correo@protonmail.com>
2022-04-16 06:55:52 -0500

    Playing with cubes I

---
+++ Java/8/POO/2_Cube/Cube.java
@@ -0,0 +1,12 @@
+public class Cube{
+    // note: use primitive data type for the side of the cube
+    private int side;
+
+    public int getSide(){
+        return this.side;
+    }
+
+    public void setSide(int newSide){
+        this.side = newSide;
+    }
+}
