45ad078aaab2c88b2ffba3098f3dec9a7d1a40f2
grayTurtle01 <the.correo@protonmail.com>
2022-05-04 06:19:02 -0500

    add: checkLine method

--- Java/4/SudokuValidator.java
+++ Java/4/SudokuValidator.java
@@ -1,4 +1,23 @@
 public class SudokuValidator{
+    
+    public static boolean checkLine(int[] line){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for( int value: line){
+            if( value == 0)
+                return false;
+            
+            if( list.contains(value) ){
+                return false;
+            }
+
+            list.add(value);
+        }
+
+        return true;
+    }
+
+
 
     public static void main(String[] args){
         System.out.println("Hello Sudoku");
