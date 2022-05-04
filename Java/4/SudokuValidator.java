142d569ef2e03af56d752ca5cfc0112544061046
grayTurtle01 <the.correo@protonmail.com>
2022-05-04 06:19:33 -0500

    add: getColumn method

--- Java/4/SudokuValidator.java
+++ Java/4/SudokuValidator.java
@@ -17,6 +17,16 @@ public class SudokuValidator{
         return true;
     }
 
+    public static int[] getColumn(int[][] m, int colNumber){
+        int n = m.length;
+        int[] column = new int[n];
+
+        for(int y = 0; y < n; y++){
+            column[y] = m[y][colNumber];
+        }
+
+        return column;
+    }
 
 
     public static void main(String[] args){
