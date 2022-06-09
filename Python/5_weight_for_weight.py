b5822e9259fdbf370723d2b23f120911693f9fc6
grayTurtle01 <the.correo@protonmail.com>
2022-06-09 12:26:36 -0500

    5: Weight-for-weight

--- Python/5_weight_for_weight.py
+++ Python/5_weight_for_weight.py
@@ -1,19 +1,24 @@
 def order_weight(strng):
+
+
     weights_str = strng.split()
+    print(weights_str)
+
+
+    weights_str = sorted(weights_str)
+    print(weights_str)
 
 
     weights = [ str(get_weight(weight)) for weight in weights_str ]
+    print(weights)
 
     sorted_list = sorted(weights_str,
                          key= lambda weight: get_weight(weight))
 
-    strng_result = " ".join(sorted_list)
-
-    print(weights_str)
-    print(weights)
-
-    print()
     print(sorted_list)
+
+
+    strng_result = " ".join(sorted_list)
     print(strng_result)
 
     return strng_result
