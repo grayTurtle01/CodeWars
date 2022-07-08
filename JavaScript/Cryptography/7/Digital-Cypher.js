9b90920db37a4b40c16f7ab4460582680840ba52
grayTurtle01 <the.correo@protonmail.com>
2022-07-08 02:26:30 -0500

    digital-cypher

---
+++ JavaScript/Cryptography/7/Digital-Cypher.js
@@ -0,0 +1,33 @@
+function Encode(str, number){
+
+    let str_number = String(number)
+    
+    let result = []
+
+    for( i in str ){
+        c = str[i]
+        value = letter2num(c)
+
+        number_index = i % str_number.length
+        number_value = str_number[number_index]
+            
+        r = value + parseInt(number_value)
+
+        result.push(r)
+    }
+
+    console.log(result)
+}
+
+
+function letter2num(letter){
+    let c = letter.toLowerCase()
+
+    let delta = c.charCodeAt() - 96
+
+    return delta
+    
+}
+
+Encode('scout', 1939)
+Encode('masterpiece', 1939)
