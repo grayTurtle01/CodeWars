d228d7e6ce7df92d9b73346d4457b5508d6f9175
grayTurtle01 <the.correo@protonmail.com>
2022-08-21 12:52:31 -0500

    Rust

---
+++ JavaScript/Cryptography/7/caesar.js
@@ -0,0 +1,67 @@
+function letter2num(letter){
+    let c = letter.toLowerCase()
+
+    let delta = c.charCodeAt() - 96
+
+    return delta
+    
+}
+
+function num2letter(number){
+    ascii_number = number + 96
+    real_character = String.fromCharCode(ascii_number)
+    return real_character
+}
+
+function encode(str, number){
+
+    let str_number = String(number)
+    
+    let result = []
+
+    for( i in str ){
+        c = str[i]
+        value = letter2num(c)
+
+        number_index = i % str_number.length
+        number_value = str_number[number_index]
+            
+        r = value + parseInt(number_value)
+
+        result.push(r)
+    }
+
+    return result
+}
+
+function decode(num_array, key){
+    let msg = ''
+
+    let key_str = String(key)
+
+    for( i in num_array ){
+
+        num = num_array[i]
+        index = i % key_str.length  
+
+        df = key_str[index]
+        
+        real_num = num - parseInt(df) 
+        real_character = num2letter(real_num)
+
+        msg += real_character
+    }
+
+    return msg
+}
+
+function caesar(str, key){
+    encoded_msg =  encode(str, key)
+    console.log(encode_msg)
+        
+    //~ msg = decode(array, key)
+    //~ console.log(msg)
+}
+
+caesar('ibm', 1)
+
---
+++ Rust/square.rs
@@ -0,0 +1,5 @@
+// Define a function "square" here which takes a signed integer of type i32
+// and returns the square of that integer
+fn square(x :i32) -> i32{
+    x * x
+} 
