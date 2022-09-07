ab2fe21b8183c701ec8604cfa278dc0fecb641a6
grayTurtle01 <the.correo@protonmail.com>
2022-09-07 09:31:20 -0500

    7_sort-by-string-length

---
+++ JavaScript/Arrays/7_sort-by-string-length.js
@@ -0,0 +1,16 @@
+// Sort array by string length
+
+// Parameters: Array of strings
+// Response:   Array of strings
+// Example:  ['abc', 'a', 'ab'] --> ['a', 'ab', 'abc']
+
+function sortByLength (array) {
+    let sortedArray = array.sort( (s1, s2) => (s1.length - s2.length) )
+
+    return sortedArray
+};
+
+v = ['abc', 'a', 'ab']
+
+console.log( sortByLength(v) )
+console.log(v[0].length)
