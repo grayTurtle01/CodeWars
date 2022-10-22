95c93522861218ac4def1b89292ecc9965ba506a
grayTurtle01 <the.correo@protonmail.com>
2022-10-22 12:26:20 -0500

    median

--- Rust/median.rs
+++ Rust/median.rs
@@ -2,8 +2,8 @@ fn main(){
 
     let mut v = vec![4,1,5,3,6,2];
     println!("{:?}", v);
-
-    v = buble_sort(v);
+    
+    v.sort();
     println!("{:?}", v);
 
     let median = get_median(v);
@@ -11,7 +11,7 @@ fn main(){
 }
 
 fn get_median(mut v: Vec<i32> ) -> f32{
-    v = buble_sort(v);
+    v.sort();
 
     if v.len() % 2 == 1{
         let mid_index = v.len()/2;
@@ -31,23 +31,3 @@ fn get_median(mut v: Vec<i32> ) -> f32{
     
 }
 
-
-fn buble_sort(mut v: Vec<i32> ) -> Vec<i32>{
-
-    let n = v.len();
-
-    for i in 0..n-1 {
-        for j in i+1..n {
-
-            if v[i] > v[j] {
-                let tmp = v[i];
-                v[i] = v[j];
-                v[j] = tmp;
-        
-            } 
-          
-        }
-    }
-
-    return v
-}
